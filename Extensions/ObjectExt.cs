0df45524e0205e390532aaac037a027e64ed35c1
Max Samson <m.samson@flawlessloop.com>
2021-06-02 01:37:59 -0600

    Creating Initial Repos

---
+++ Extensions/ObjectExt.cs
@@ -0,0 +1,49 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCodeDev.Forms.Grpc.Extensions
+{
+    public static class ObjectExt
+    {
+        /// <summary>
+        /// Validate EditContext DataAnnotations<br/>
+        /// Note: Server-Side should never result to error. The only way for validation error occurs server-side its either (attack) or mistake from client side. Validation form should be shared with server-client.
+        /// </summary>
+        /// <param name="throwOnError">True = Will throw GRPC InvalidArgument error if fails.</param>
+        public static bool ValidateForm(this object context, bool throwOnError = true)
+        {
+            try
+            {
+                var ctx = new ValidationContext(context);
+                bool rez = Validator.TryValidateObject(context, ctx, null, true);
+                if (!rez)
+                {
+                    if (throwOnError)
+                    {
+                        throw new Exception();
+                    }
+                    else
+                    {
+                        return rez;
+                    }
+
+                }
+                else
+                {
+                    return rez;
+                }
+            }
+            catch
+            {
+
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Error cannot validate the form."));
+            }
+
+        }
+    }
+}
---
+++ Extensions/ProtoConversionExt.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCodeDev.Forms.Grpc.Extensions
+{
+    public static class ProtoConversionExt
+    {
+    /// <summary>
+    /// Convert EfCore Table to Client Equivalent. <br/>
+    /// Note: System simply convert Entity Framework Table to Grpc Message Object using Json.<br/>
+    /// Note 2: I could not find a quick way to achieve json-like conversion with grpc.... if you have any idea or a better and faster way to achieve it feel free to contribute.
+    /// </summary>
+    /// <typeparam name="T">Target Object</typeparam>
+    /// <returns></returns>
+        public static T ToProtoMessage<T>(this object obj)
+        {
+            try
+            {
+                JsonSerializerSettings f = new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+                return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(obj, f));
+            }
+            catch (Exception ex)
+            {
+                throw new RpcException(new Status(StatusCode.Internal, "Fatal Internal Error, This has been reported for further investigation."));
+            }
+        }
+    }
+}
